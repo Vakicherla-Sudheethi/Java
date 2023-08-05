f5e6fd8ff45137fbf6735f6eddabffe77d002ab0
Vakicherla Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2023-08-05 19:08:22 +0530

    Create Boxing.java

---
+++ Boxing.java
@@ -0,0 +1,9 @@
+// BOXING -- wrapper
+class HelloWorld {
+    public static void main(String[] args) {
+        int num = 7;
+        Integer num1 = new Integer(8);
+        
+        System.out.println(num1);
+    }
+}
