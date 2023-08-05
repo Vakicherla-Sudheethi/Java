377633c3adf457014769f75ffb291d6e0205ffd1
Vakicherla Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2023-08-05 19:15:56 +0530

    Create Auto Unboxing.java

---
+++ Auto Unboxing.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int num = 7;
+        Integer num1 = num;//auto-boxing
+        
+        int num2 = num1;//auto-unboxing
+        System.out.println(num2);
+    }
+}
