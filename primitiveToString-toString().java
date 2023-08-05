2c965b10adb08766fb50fb84353a4a55777d05b2
Vakicherla Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2023-08-05 19:24:43 +0530

    Create primitiveToString-toString().java

---
+++ primitiveToString-toString().java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int n=2;
+        Integer n1 = new Integer(n);
+        String s = n1.toString();
+        System.out.println(s);
+    }
+}
