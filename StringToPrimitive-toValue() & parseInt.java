93337cbee08e55f2a58567f5e14c4dc69ceded0b
Vakicherla Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2023-08-05 19:30:18 +0530

    Create StringToPrimitive-toValue() & parseInt.java

---
+++ StringToPrimitive-toValue() & parseInt.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        //conversion from string to primitive
+        //using valueOf
+        String s = "2";
+        int n = Integer.valueOf(s);
+        System.out.println(n);
+        //using Xxxvalue
+        String a = "4";
+        int m = Integer.parseInt(a);
+        System.out.println(m);
+        
+    }
+}
