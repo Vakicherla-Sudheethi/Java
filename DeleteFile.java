adf22119b2e4f554d2e91499263fef84b7edbea6
Vakicherla Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2023-08-08 21:11:32 +0530

    DeleteFile.java

--- DeleteFile.java
+++ DeleteFile.java
@@ -3,7 +3,7 @@ import java.io.File;
 class DeleteFile {
     public static void main(String args[]) {
         File f = new File("C:\\Users\\fluff\\OneDrive\\Desktop\\Java\\Directory");
-        if(!f.delete())
+        if(f.delete())
 {
 System.out.println("deleted");
 }
