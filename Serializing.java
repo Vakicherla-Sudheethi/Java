07167495906f44d614d6e0ab54fd82766ec65f8e
Vakicherla Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2023-08-08 18:44:14 +0530

    Create Serializing.java

---
+++ Serializing.java
@@ -0,0 +1,22 @@
+public class Serializing {
+
+   public static void main(String [] args) {
+      Employee e = new Employee();
+      e.name = "Ram";
+      e.address = "Bow bow,meaow meaow";
+      e.SSN = 11122333;
+      e.number = 101;
+      
+      try {
+         FileOutputStream fileOut =
+         new FileOutputStream("/tmp/employee.ser");
+         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+         out.writeObject(e);
+         out.close();
+         fileOut.close();
+         System.out.printf("Serialized data is saved in /tmp/employee.ser");
+      } catch (IOException i) {
+         i.printStackTrace();
+      }
+   }
+}
