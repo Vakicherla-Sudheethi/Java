e2b2c51051f47af7bc5e3e7e06ebeb106b510397
Vakicherla Sudheethi <82566352+Vakicherla-Sudheethi@users.noreply.github.com>
2023-08-08 21:09:44 +0530

    Add files via upload

---
+++ DeleteFile.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+class DeleteFile {
+    public static void main(String args[]) {
+        File f = new File("C:\\Users\\fluff\\OneDrive\\Desktop\\Java\\Directory");
+        if(!f.delete())
+{
+System.out.println("deleted");
+}
+else
+{
+System.out.println("not deleted");
+}
+}
+}
+
+//deleted
